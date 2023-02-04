cf311ead12bc560a071fdbe0d6008060369834d4
lruizap <lruizap2502@gmail.com>
2023-02-04 12:28:42 +0100

    Trabajando con los hooks "useEffect" y "useState"

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function Counter() {
   const [message, setMessage] = useState("");
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("render");
+  }, [counter]);
 
   return (
     <div>
@@ -12,6 +17,16 @@ export function Counter() {
         }}
       >
         Save
+      </button>
+      <hr></hr>
+
+      <h1>Counter {counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Incrementar
       </button>
     </div>
   );
